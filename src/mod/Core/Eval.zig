c276ac23a1ff930dc994d70c7103e201090de85c
noxabellus <noxabellus@gmail.com>
2024-09-10 19:58:20 -0500

    woops, should have left always inline for the root decoder call

--- src/mod/Core/Eval.zig
+++ src/mod/Core/Eval.zig
@@ -52,7 +52,7 @@ pub fn stepBytecode(fiber: *Fiber, localData: RegisterData, upvalueData: ?Regist
         .offset = &blockFrame.ip_offset,
     };
 
-    const instr = try decoder.decode(Bytecode.Op);
+    const instr = try @call(.always_inline, IO.Decoder.decode, .{&decoder, Bytecode.Op});
 
     switch (instr) {
         .trap => return Fiber.Trap.Unreachable,
